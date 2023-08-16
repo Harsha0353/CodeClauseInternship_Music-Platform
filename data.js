f6d416b31ac3276db81d2a90fd6589ae9327525a
Harsha Vardhan <108411057+Harsha0353@users.noreply.github.com>
2023-08-16 14:58:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'Naa Ready',
+        path: 'music/Song 1.mp3',
+        artist: 'Anirudh . Thalapathy Vijay . Asal Kolaar',
+        cover: 'images/leo.jpg'
+    },
+    {
+        name: 'Alappara Theme',
+        path: 'music/Song 2.mp3',
+        artist: 'Anirudh',
+        cover: 'images/ak.jpeg'
+    },
+    {
+        name: 'Hukum',
+        path: 'music/Song 3.mp3',
+        artist: 'Anirudh . Super Subu',
+        cover: 'images/hukum.jpeg'
+    },
+    {
+        name: 'Jailer Drill Theme',
+        path: 'music/Song 4.mp3',
+        artist: 'Anirudh',
+        cover: 'images/Jailer.jpg'
+    },
+    {
+        name: 'Jailer Theme',
+        path: 'music/Song 5.mp3',
+        artist: 'Anirudh',
+        cover: 'images/img2.jpg'
+    },
+    {
+        name: 'Muthuvel Pandian Theme',
+        path: 'music/Song 6.mp3',
+        artist: 'Anirudh',
+        cover: 'images/img1.jpeg'
+    },
+    {
+        name: 'Jai Shri Ram',
+        path: 'music/Song 7.mp3',
+        artist: 'Ajay-Atul . Manoj Muntashir',
+        cover: 'images/Adipurush.jpg'
+    },
+]
